d74805cd85548e6553e094e22228222af905a43a
RntbotBR <78214269+RntbotBR@users.noreply.github.com>
2021-01-29 17:39:54 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do 🔥☠︎︎𝗥⃟𝗡𝗧BOT☠︎︎͜͡🇧🇷🔥*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
